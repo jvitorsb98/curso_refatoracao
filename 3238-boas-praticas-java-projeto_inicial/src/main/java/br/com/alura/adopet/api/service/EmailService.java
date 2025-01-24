7822fcbdddd5a5d92d46ca7b90be99e050bceb95
João Ramos <jvitorsb98@outlook.com>
2025-01-24 17:42:14 -0300

    Aula 01 - curso 2

---
+++ 3238-boas-praticas-java-projeto_inicial/src/main/java/br/com/alura/adopet/api/service/EmailService.java
@@ -0,0 +1,23 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.model.Adocao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+
+    public void send(String to , String subject , String message){
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setFrom("adopet@email.com.br");
+        email.setTo(to);
+        email.setSubject(message);
+        email.setText(message);
+        emailSender.send(email);
+    }
+
+}
